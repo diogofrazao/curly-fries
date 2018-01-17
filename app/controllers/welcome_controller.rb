90d069a2df7367a8e8d5f359ba56e65348bbd228
Diogo Frazao <diogo.frazao@marleyspoon.com>
2018-01-17 16:12:42 +0100

    add more bad code

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,7 @@ class WelcomeController < ApplicationController
     @ketchup = Ketchup.new("red", 2)
     duplicate_code_1(4)
     duplicate_code_2(3)
+    bad_code
   end
 
   private
@@ -17,5 +18,50 @@ class WelcomeController < ApplicationController
     new_number = number * 2
     new_number = new_number + 2 * 5
     new_number = 10
+  end
+
+  def bad_code
+    begin
+      "hey" if [1,2,3].empty?
+    rescue Exception
+     "hehehehehe"
+    end
+  end
+
+  def unused_code
+    "hey hey hey"
+  end
+
+  def big_function
+    lol = 5
+    lol = 7
+    lol = 9
+    lol = 5
+    lol = 7
+    lol = 9
+    lol = 5
+    lol = 7
+    lol = 9
+    lol = 5
+    lol = 7
+    lol = 9
+    lol = 5
+    lol = 7
+    lol = 9
+    lol = 5
+    lol = 7
+    lol = 9
+    lol = 5
+    lol = 7
+    lol = 9
+    lol = 5
+    lol = 7
+    lol = 9
+    lol = 5
+    lol = 7
+    lol = 9
+    lol = 5
+    lol = 7
+    lol = 9
   end
 end
